dcb1328019a87fd11776583da2fc18c99a5b5505
xiao-bo <ay852852@gmail.com>
2022-03-02 21:59:58 +0800

    feature: 拉架構

--- libs/schedule/scheduler.py
+++ libs/schedule/scheduler.py
@@ -10,9 +10,37 @@ class Scheduler(object):
         self.class_schedule = ClassSchedule()
         self.TeacherSchedule = TeacherSchedule()
         self.ScheduleBuilder = ScheduleBuilder()
+
     def runner(self):
         print('Scheduler runner')
-        self.gsc.runner()        
-        self.class_schedule.runner()
-        self.TeacherSchedule.runner()
+        self.build_class_schedule()
+
+
+    def build_class_schedule(self):
+        self.__read_teacher_subject()
+        #self.gsc.runner()        
+        #self.class_schedule.runner()
+        #self.TeacherSchedule.runner()
         self.ScheduleBuilder.runner()
+        
+    def transfer_class_schedule_to_teacher_schedule(self):
+        pass
+
+    def __read_teacher_subject(self):
+        pass 
+
+    def __read_teacher_requirement(self):
+        pass
+
+    def __read_leave_date_of_teacher(self):
+        pass
+
+    def __read_class_counts_be_arrange(self):
+        pass 
+
+    def __read_counts_of_subject(self):
+        pass  
+
+    def __read_general_config(self):
+        pass 
+    
