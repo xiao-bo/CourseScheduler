a14a67b91d77d3515a0fbbb10953117ffb7e51d7
xiao-bo <ay852852@gmail.com>
2022-03-02 22:02:22 +0800

    feature: 拉架構

--- libs/schedule/schedule_builder.py
+++ libs/schedule/schedule_builder.py
@@ -5,3 +5,20 @@ class ScheduleBuilder(object):
 
     def runner(self):
         print('this is ScheduleBuilder runner')
+        self.build_schedule()
+
+    def build_schedule(self):
+        pass
+
+    def __arrange_course(self):
+        pass 
+
+    def __check_arrange(self):
+        pass 
+
+    def __check_requirements_of_teacher(self):
+        pass 
+
+    def __check_reqirements_of_class(self):
+        pass 
+    
